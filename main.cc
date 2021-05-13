25c718eba227a42119f966870699ecfabcf48be7
Nyoungg <nellyyoung@csu.fullerton.edu>
2021-05-13 06:41:09 +0000

    finished

---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "game.h"
+#include "game_element.h"
+#include "opponent.h"
+#include "player.h"
+
+int main() {
+  Game game1;
+  game1.Init();
+  game1.Start();
+  return 0;
+}
